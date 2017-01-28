5de3f4ea3e3b07b22377a1e7758fb90514a7d308
Jimmy Riccelli <jrriccelli@gmail.com>
2017-01-28 14:06:13 -0500

    connected mlab db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var expressLayouts = require('express-ejs-layouts');
+var mongoose = require('mongoose');
 var morgan =require('morgan');
 var port = process.env.PORT || 8080;
 
@@ -12,6 +13,8 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static(__dirname + '/public'));
 
+//connect to db
+mongoose.connect('mongodb://scotch:scotch@ds135519.mlab.com:35519/olympic-events');
 //set routes
 var router = require('./app/routes');
 app.use('/',router);
